2d9601404de08261a0bc273406449d88dc7f572e
santoshini <santoshinipradhan01@gmail.com>
2024-12-15 18:24:35 +0530

    styles added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="p-4 flex items-center justify-between rounded-bl-3xl rounded-br-3xl bg-gradient-to-r from-white/40 to-blue-50 backdrop-blur-lg shadow-lg border-b border-gray-200">
-        <p> Made with <span> <FaHeart style={{ color: 'red', fontSize: '2rem' }} /></span> by Santoshini.. </p>
+    <div className="p-4 bg-gradient-to-r from-white/40 to-blue-50 backdrop-blur-lg shadow-lg border-t border-gray-200">
+      <div className="flex justify-center items-center text-center">
+        <span>Made with</span> <FaHeart style={{ color: 'red', fontSize: '1.5rem', margin: '0 5px' }} />by Santoshini
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
